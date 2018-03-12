87c512b88d1ee437dcda88544ef542e7386f68f5
L. D. Nicolas May <LDNicolasMay@gmail.com>
2018-03-12 09:22:31 -0400

    reshape data long and wide

--- CuesCogstateToolboxUds.R
+++ CuesCogstateToolboxUds.R
@@ -16,6 +16,7 @@ length(unique(df$madc_id))
 which(duplicated(df$subject_id) == duplicated(df$mrn) %in% FALSE) + 1
 which(duplicated(df$subject_id) == duplicated(df$madc_id) %in% FALSE) + 1
 which(duplicated(df$mrn) == duplicated(df$madc_id) %in% FALSE) + 1
+which(duplicated(df$subject_id) == duplicated(df$dob) %in% FALSE) + 1
 
 #####
 ### Check dates
@@ -26,7 +27,7 @@ length(grep(pattern = "date", x = names(df)))
 
 # Coerce df$dob to type Date
 class(df$dob)
-lubridate::mdy(df$dob)
+# lubridate::mdy(df$dob)
 df$dob <- lubridate::mdy(df$dob)
 
 # Coerce any columns with "date" in field name to type Date
@@ -57,20 +58,45 @@ df_dates_future <- df_dates %>%
 lapply(X = df_dates_future[, -(1:2)], FUN = function(x) { sum(x > Sys.Date(), na.rm = TRUE) })
 lapply(X = df_dates_future[, -(1:2)], FUN = function(x) { x > Sys.Date() })
 
+# Shorten redcap_event_name values
+df$redcap_event_name <- stringr::str_replace(string = df$redcap_event_name, pattern = "visit_", replacement = "v")
+df$redcap_event_name <- stringr::str_replace(string = df$redcap_event_name, pattern = "_arm_1", replacement = "")
+
+# Reshape to long
+df_long <- df %>% 
+  dplyr::select(subject_id, mrn, madc_id, dob, redcap_event_name, everything()) %>% 
+  tidyr::gather(key = "var", value = "val", -subject_id, -redcap_event_name, -mrn, -madc_id, -dob) 
+
+# Reshape to wide
+df_wide <- df_long %>% 
+  tidyr::unite(col = "redcap_event_name.var", redcap_event_name, var, sep = ".") %>%
+  tidyr::spread(key = redcap_event_name.var, value = val)
+
+# Sample wide data set (for SPSS) with particular values: 
+#   subject_id, age, uds_consensus_dx, education, cog_crys_*
+df_wide_sample <- df_long %>% 
+  dplyr::filter(var == "subject_id" | var == "age" | var == "uds_consensus_dx" | 
+                  var == "cog_crys_comp_uss" | var == "cog_crys_comp_aass" | 
+                  var == "cog_crys_comp_npaa" | var == "cog_crys_comp_fass") %>% 
+  tidyr::unite(col = "redcap_event_name__var", redcap_event_name, var, sep = "__") %>% 
+  tidyr::spread(key = redcap_event_name__var, value = val)
+
+### Might be useful to partition the data by column types (numerical, date, character) and then cbind
+
 #####
 ### Check columns with numeric values
 ##
 # df_num
-df_num <- df %>% 
-  select_if(is.numeric) 
-df_num_range <- lapply(X = df_num, FUN = function(x) { range(x, na.rm = TRUE) })
+# df_num <- df %>% 
+#   select_if(is.numeric) 
+# df_num_range <- lapply(X = df_num, FUN = function(x) { range(x, na.rm = TRUE) })
 
 ####
-###
+### Check columns with character values
 ##
 #
-df_char <- df %>% 
-  select_if(is.character)
+# df_char <- df %>% 
+#   select_if(is.character)
 
 # df_factor <- df %>% # no factor columns
 #   select_if(is.factor)
